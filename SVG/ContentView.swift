b5fe9c00c7773638124749b21d42a981b68f1fac
twber <hctsai@umich.edu>
2020-08-02 03:59:10 +0800

    Initial Commit

---
+++ SVG/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SVG
+//
+//  Created by Bernice TSAI on 2020/8/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
