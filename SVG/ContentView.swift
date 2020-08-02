4a555e93d597e2ba54078f682e281ffaf5467e17
twber <hctsai@umich.edu>
2020-08-02 14:10:33 +0800

    The converted images did not appear.

--- SVG/ContentView.swift
+++ SVG/ContentView.swift
@@ -1,20 +1,25 @@
-//
-//  ContentView.swift
-//  SVG
-//
-//  Created by Bernice TSAI on 2020/8/2.
-//
 
 import SwiftUI
-
 struct ContentView: View {
+    @State var endAmount: CGFloat = 0
+    @State var isFilled = false
+    let ctColor1 = UIColor.fromHexString("#F88A36")
+    let ctColor2 = UIColor.fromHexString("#FD2020")
+    let pathBounds = UIBezierPath.calculateBounds(paths: [.ctLogo1, .ctLogo2])
     var body: some View {
-        Text("Hello, world!").padding()
+    ZStack{
+        ShapeView(bezier: .ctLogo1, pathBounds: pathBounds)
+        .trim(from:0, to:endAmount)
+            .stroke(Color(ctColor1), lineWidth: 2)
+        ShapeView(bezier: .ctLogo2, pathBounds: pathBounds)
+            .fill(Color(ctColor2).opacity(isFilled ? 1:0))
     }
+    .frame(width: 300, height:300*pathBounds.height/pathBounds.width)
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+static var previews: some View {
+    ContentView()
+}
 }
